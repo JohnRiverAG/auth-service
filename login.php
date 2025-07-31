dd1a94a3e5c738c7241e47f508b9e847a14fa21f
John Arango <johnunivalle@gmail.com>
2025-08-01 01:34:09 +0200

    Login de usuarios implementado

--- login.php
+++ login.php
@@ -1,2 +1,30 @@
 
 
+<?php
+    require 'db.php';
+
+    //Capturamos los datos ingresados
+    $data = json_decode(file_get_contents("php://input"), true);
+    $username = $data['username'] ?? '';
+    $password = $data['password'] ?? '';
+
+    //Verificamos que no hayan campos vacios
+    if (!$username || !$password) {
+        http_response_code(400);
+        echo json_encode(['error' => 'Usuario y contraseña requeridos']);
+        exit;
+    }
+
+    //Corroboramos ingresados con los almacenados
+    $stmt = $pdo->prepare("SELECT password FROM users WHERE username = ?");
+    $stmt->execute([$username]);
+    $user = $stmt->fetch();
+
+    //COmunicamos el resultado de la consulta
+    if ($user && password_verify($password, $user['password'])) {
+        echo json_encode(['message' => 'Autenticación satisfactoria']);
+    } else {
+        http_response_code(401);
+        echo json_encode(['error' => 'Error en la autenticación']);
+    }
+?>
